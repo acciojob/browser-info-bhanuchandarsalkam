ce4f68625c5902e396c5f1c734a9c127bfad0db6
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-06-29 21:29:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
 const info=document.getElementById("browser-info");
-info.innerText="you are using Netscape version 5.0";
+info.innerText="You are using Netscape version 5.0";
